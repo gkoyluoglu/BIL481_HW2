60d8d49cb477d9038e7ad910e3bd5244ec2ddd52
gkoyluoglu <47077097+gkoyluoglu@users.noreply.github.com>
2019-03-06 23:46:40 +0300

    Add files via upload
    Addint TestCompute.java

--- branchCoverage/my-app/src/test/java/TestCompute.java
+++ branchCoverage/my-app/src/test/java/TestCompute.java
@@ -9,9 +9,30 @@ public class TestCompute {
   Compute c;
 
   @Test
-  public void example() {
+  public void checkSizeZero() {
     MessageQueue mq = mock(MessageQueue.class);
     c = new Compute(mq);
-    assertTrue(true);
+    when(mq.size()).thenReturn(0);
+    assertEquals(c.countNumberOfOccurrences("11654") , -1);
   }
+  @Test
+  public void checkContains() {
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+    when(mq.contains("asdas")).thenReturn(false);
+    when(mq.size()).thenReturn(2);
+    assertTrue(c.countNumberOfOccurrences("asdas") == 0);
+  }
+  @Test
+  public void check(){
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+    when(mq.size()).thenReturn(3);
+    when(mq.contains("abc")).thenReturn(true);
+    when(mq.getAt(0)).thenReturn("abc");
+    when(mq.getAt(1)).thenReturn("abc");
+    when(mq.getAt(2)).thenReturn("abc");
+    assertTrue(c.countNumberOfOccurrences("abc") == 3);
+  }
+
 }
