de8ff647431237462b6767eaf3aed1a5c880b847
gkoyluoglu <47077097+gkoyluoglu@users.noreply.github.com>
2019-03-06 23:47:28 +0300

    Adding TestUtil.java

--- branchCoverage/my-app/src/test/java/TestUtil.java
+++ branchCoverage/my-app/src/test/java/TestUtil.java
@@ -6,11 +6,33 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestUtil {
-  Util c;
+    Util c;
 
   @Before
   public void setUp() { c = new Util(); }
-
   @Test
-  public void example() { assertTrue(true); }
+  public void lengthOne() {
+      assertFalse(c.compute(1));
+  }
+  @Test
+  public void isEven() {
+      assertFalse(c.compute(1, 2));
+      assertFalse(c.compute(1, 2, 3, 4));
+      assertFalse(c.compute(1, 23, 4, 5, 6, 5));
+      assertFalse(c.compute(1, 2, 4, 3, 5, 6, 7, 8));
+  }
+  @Test(expected=RuntimeException.class)
+  public void testException() {
+      c.compute(0, 1, 23, 4654, 15);
+  }
+  @Test
+  public void check() {
+      assertTrue(c.compute(2, 3, 4, 5, 6, 7, 8));
+      assertFalse(c.compute(2, 5));
+  }
+    @Test
+    public void isFalse() {
+        assertFalse(c.compute());
+        assertFalse(c.compute());
+    }
 }
